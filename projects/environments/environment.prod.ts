870daae5edc17257fa4396b7b5adc16178d43ddb
Augusto Mentz <augusto@flowrms.com>
2025-08-27 23:56:35 -0300

    env

--- projects/environments/environment.prod.ts
+++ projects/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   api: {
-    catalog: 'http://34.95.241.39:8000',
-    cart: 'http://34.95.206.140:8001',
-    checkout: 'http://35.198.3.27:8002'
+    catalog: 'http://34.117.216.147/catalog',
+    cart: 'http://34.117.216.147/cart',
+    checkout: 'http://34.117.216.147/checkout'
   },
   mfe: {
-    catalog: 'http://34.54.141.101/catalog',
+    catalog: 'http://34.49.44.239/catalog',
     cart: 'http://localhost:4201',
     checkout: 'http://localhost:4203'
   }
